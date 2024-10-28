207fe12f288ffdb8d25541c51a093dec1b50cb09
Ivanete <ivanetevieira1000@gmail.com>
2024-10-28 19:58:09 -0300

    Implementação das configurações unificadas do .NET 6+ atualizadas em Program

--- Program.cs
+++ Program.cs
@@ -1,19 +1,71 @@
-using Dev.visitante.Startup;
-using Microsoft.AspNetCore.Hosting;
+using Dev.visitante.Aplication.Services;
+using Dev.visitante.Handlers;
+using Dev.visitante.Infrastructe.Persitence;
+using Dev.visitante.Infrastructe.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do banco de dados
+builder.Services.AddDbContext<PessoaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")!)
+);
+
+// Configuração dos serviços da aplicação
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IPessoaHandlerException, PessoaHandlerException>();
+builder.Services.AddScoped<IPessoaService, PessoaService>();
+builder.Services.AddScoped<IPessoaRepository, PessoaRepository>();
+
+// Configuração do Swagger
+builder.Services.AddSwaggerGen(o =>
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults
-        (webBuilder =>
+    o.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "Visitante.Api",
+            Version = "v1",
+            Contact = new OpenApiContact
             {
-                webBuilder.UseStartup<Startup>();
-            });
-            
+                Name = "Ivanete Silva",
+                Email = "ivanetevieira1000@gmail.com"
+            }
+        }
+    );
+});
+
+var app = builder.Build();
+
+// Configuração do pipeline de tratamento de requisições
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Visitante.Api v1");
+    });
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{id?}",
+    defaults: new { controller = "Pessoas", action = "Index" }
+);
+
+app.Run();
