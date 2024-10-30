13f57e8525e80974b42ddea4677c39de4b86f703
Ivanete <ivanetevieira1000@gmail.com>
2024-10-30 17:01:51 -0300

    Adicionar projeto Blazor para interface de gerenciamento de visitantes

---
+++ VisitanteApp.Blazor/Models/Pessoa.cs
@@ -0,0 +1,11 @@
+namespace VisitanteApp.Blazor.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Profissao { get; set; }
+        public string? Descricao { get; set; }
+        public int Idade { get; set; }
+    }
+}
